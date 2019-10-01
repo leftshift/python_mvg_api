d5f4e4d03afac8b07240822bbae63b29b1044ace
Adrian Schneider <adrian.schneider@abusix.com>
2019-10-01 14:57:29 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='mvg_api',
-    version='1.1.2',
+    version='1.2.0',
     description='Library for fetching departure and route data from the Munich public transport organisation MVG',
     long_description=long_description,
     url='https://github.com/leftshift/python_mvg_api',
